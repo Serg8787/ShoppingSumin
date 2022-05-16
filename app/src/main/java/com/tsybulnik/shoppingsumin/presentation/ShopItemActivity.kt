8cf3618b14a39644b92ec79e599a89d056e5bfbc
Serg8787 <tsybulnikserg@gmail.com>
2022-05-16 12:58:43 +0300

    prepare shopItemAc

---
+++ app/src/main/java/com/tsybulnik/shoppingsumin/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.tsybulnik.shoppingsumin.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tsybulnik.shoppingsumin.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
