f688d165b5d66ce066315b46322c571d73bf7feb
Serg8787 <tsybulnikserg@gmail.com>
2022-05-17 22:31:23 +0300

    add methods newIntent

--- app/src/main/java/com/tsybulnik/shoppingsumin/presentation/MainActivity.kt
+++ app/src/main/java/com/tsybulnik/shoppingsumin/presentation/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.tsybulnik.shoppingsumin.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.tsybulnik.shoppingsumin.R
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
@@ -20,6 +21,12 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
             adapter.submitList(it)
+        }
+
+        val buttonAddShopItem = findViewById<FloatingActionButton>(R.id.button_add_shop_item)
+        buttonAddShopItem.setOnClickListener {
+            val intent = ShopItemActivity.newIntentAddItem(this)
+            startActivity(intent)
         }
     }
 
@@ -63,7 +70,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupClickListener() {
         adapter.onShopItemClickListener = {
-            Log.d("MyLog", it.id.toString())
+            val intent = ShopItemActivity.newIntentEditItem(this, it.id)
+            startActivity(intent)
         }
     }
 
--- app/src/main/java/com/tsybulnik/shoppingsumin/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/tsybulnik/shoppingsumin/presentation/ShopItemActivity.kt
@@ -1,17 +1,44 @@
 package com.tsybulnik.shoppingsumin.presentation
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import com.tsybulnik.shoppingsumin.R
+import com.tsybulnik.shoppingsumin.domain.ShopItem
 
 class ShopItemActivity : AppCompatActivity() {
-    private lateinit var viewModel: ShopItemViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
 
+        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
+        Log.d("extra_mode",mode.toString())
 
-        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+
+
+    }
+
+    companion object{
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val SHOP_ITEM_ID = "extra_shopItem_id"
+        private const val MODE_EDIT = "mode_edit"
+        private const val MODE_ADD = "mode_add"
+
+        fun newIntentAddItem(context: Context):Intent{
+            val intent = Intent(context,ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+            return intent
+        }
+
+        fun newIntentEditItem(context: Context,shopItemId:Int):Intent{
+            val intent = Intent(context,ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+            intent.putExtra(SHOP_ITEM_ID,shopItemId)
+            return intent
+        }
     }
 }
