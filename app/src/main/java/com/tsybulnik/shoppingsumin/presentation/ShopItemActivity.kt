bf916351ee9f33cb40a9afdc62b36db984e66e6a
Serg8787 <tsybulnikserg@gmail.com>
2022-05-17 23:15:02 +0300

    add methods newIntent1

--- app/src/main/java/com/tsybulnik/shoppingsumin/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/tsybulnik/shoppingsumin/presentation/ShopItemActivity.kt
@@ -2,42 +2,95 @@ package com.tsybulnik.shoppingsumin.presentation
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.textfield.TextInputLayout
 import com.tsybulnik.shoppingsumin.R
 import com.tsybulnik.shoppingsumin.domain.ShopItem
 
 class ShopItemActivity : AppCompatActivity() {
+    private lateinit var tilName: TextInputLayout
+    private lateinit var tilCount: TextInputLayout
+    private lateinit var etName: EditText
+    private lateinit var etCount: EditText
+    private lateinit var btSave: Button
+
+    private lateinit var viewModel: ShopItemViewModel
+
+    private var screenMode = MODE_UNKNOWN
+    private var shopItemID = ShopItem.UNDEFINED_ID
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+        parseIntent()
 
-        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
-        Log.d("extra_mode",mode.toString())
+        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+
+        initViews()
+        when(screenMode){
+            MODE_EDIT -> launchEditMode()
+            MODE_ADD -> launchAddMode()
+
+        }
+
+
 
 
 
     }
+    private fun launchEditMode(){
 
-    companion object{
+    }
+    private fun launchAddMode(){
+
+    }
+
+    private fun initViews() {
+        tilName = findViewById(R.id.til_name)
+        tilCount = findViewById(R.id.til_count)
+        etName = findViewById(R.id.et_name)
+        etCount = findViewById(R.id.et_count)
+        btSave = findViewById(R.id.button_add_shop_item)
+    }
+
+    private fun parseIntent() {
+        if (!intent.hasExtra(EXTRA_SCREEN_MODE)) {
+            throw RuntimeException("PAram screen is absent")
+        }
+        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
+        if (mode != MODE_ADD && mode != MODE_EDIT) {
+            throw RuntimeException("UNrnown screen mode $mode")
+        }
+        screenMode = mode
+        if (screenMode == MODE_EDIT) {
+            if (!intent.hasExtra(SHOP_ITEM_ID)) {
+                throw  RuntimeException("Param shop item ID is not found")
+            }
+            shopItemID = intent.getIntExtra(SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
+        }
+    }
+
+    companion object {
         private const val EXTRA_SCREEN_MODE = "extra_mode"
         private const val SHOP_ITEM_ID = "extra_shopItem_id"
         private const val MODE_EDIT = "mode_edit"
         private const val MODE_ADD = "mode_add"
+        private const val MODE_UNKNOWN = ""
 
-        fun newIntentAddItem(context: Context):Intent{
-            val intent = Intent(context,ShopItemActivity::class.java)
+        fun newIntentAddItem(context: Context): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
             intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
             return intent
         }
 
-        fun newIntentEditItem(context: Context,shopItemId:Int):Intent{
-            val intent = Intent(context,ShopItemActivity::class.java)
+        fun newIntentEditItem(context: Context, shopItemId: Int): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
             intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
-            intent.putExtra(SHOP_ITEM_ID,shopItemId)
+            intent.putExtra(SHOP_ITEM_ID, shopItemId)
             return intent
         }
     }
